bf7f9cfe90c01046cfe9e5dd623052d6b00b8dc2
KD-Scripts <116671755+KD-Scripts@users.noreply.github.com>
2022-10-25 23:16:31 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+author 'Kokkachi Damu#3544'
+description 'KD_ServerAnnounce For Spadikam RolePlay Its An Audio Announce For All'
+
+shared_script 'config.lua'
+-- Server Scripts
+server_script {
+    'server/server.lua'
+}
+-- Client Scripts
+client_script {
+    'client/client.lua'
+}
+
