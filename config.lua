73d4135084a1fb51dfd51d708590cb99a9103d98
KD-Scripts <116671755+KD-Scripts@users.noreply.github.com>
2022-10-25 23:28:47 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,20 +5,6 @@ Config.Cmd2 = "ssd" --Shutdown
 Config.Cmd3 = "smt" --maintenance
 
 Config.AdminList = { --add admin Identifiers here These Players can only use the commands
-    'discord:717700342694936676', --Kokkachi
-	'discord:754551134726651946', --Max
-    'discord:751881120085442581', --luttu
-    'discord:851707147741036544', --Akilesh
-    'discord:783960329209249802', --Drox
-    'discord:745192468458504202', --satan
-    'discord:868761317526355989', --Mallu
-    'discord:754551134726651946', --Devildra
-    'discord:612631914305552384', --Maddy
-    'discord:754551134726651946', --Jerin
-    'discord:863152471877746699', --bob
-    'discord:921230169543409764', --tgs
-    'discord:987759278398013511', --arun
-    'discord:727486875727822958', --arun
-    'discord:749301869742129183', --doctor
-    'discord:855548294209208331' --ibilis
+	'discord:6574654465765465',
+	'steam:ksghs45bh32x75ndxfh'
 }
